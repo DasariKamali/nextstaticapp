f914355aab1ec612ec76870c8ccedaae9ec00516
Dasari Kamali (LTIMINDTREE LIMITED) <v-dkamali@microsoft.com>
2024-10-21 16:47:32 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  output: 'export',
+  // Remove basePath to avoid adding '/blog' to all routes
+  // basePath: '/blog',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/blog', // Redirect root to /blog
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,16 @@
+// src/pages/blog/index.tsx
+import Head from "next/head";
+
+export default function Blog() {
+  return (
+    <>
+      <Head>
+        <title>Blog</title>
+        <meta name="description" content="Welcome to the blog!" />
+      </Head>
+      <main>
+        <p>Welcome to the Blog!</p>
+      </main>
+    </>
+  );
+}
