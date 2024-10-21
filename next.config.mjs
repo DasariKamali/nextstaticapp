a5eac35c8311666b35ab850ec8eb44b30187dd8f
DasariKamali <107987424+DasariKamali@users.noreply.github.com>
2024-10-21 17:33:03 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,7 @@
-// /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
-  },
-  output: 'export',
-  // Remove basePath to avoid adding '/blog' to all routes
-  // basePath: '/blog',
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/blog', // Redirect root to /blog
-        permanent: false,
-      },
-    ];
   },
 };
 
